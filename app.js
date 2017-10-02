e544f113c2ad4764debd7919fcbff5ffafa40b05
Mark Allen <mark@traversify.com>
2017-10-02 09:21:12 +0200

    added cors and mongoose

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+const index = require('./routes/index');
 
-var app = express();
+const app = express();
+
+mongoose.connect('mongodb://localhost/app-beer-builder');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,16 +24,18 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 
 app.use('/', index);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+  const err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
+
+
 
 // error handler
 app.use(function(err, req, res, next) {
@@ -38,9 +43,12 @@ app.use(function(err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
+  console.log(req.path, req.method, err);
   // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+  if (!res.headersSent) {
+    res.status(err.status || 500);
+    res.render('error');
+  }
 });
 
 module.exports = app;
